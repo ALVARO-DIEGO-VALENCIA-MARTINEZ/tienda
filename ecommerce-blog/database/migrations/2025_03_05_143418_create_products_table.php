3032759aa9433d71752fa1bc43b08488f18cfdf1
ALVARO-DIEGO-VALENCIA-MARTINEZ <alvarodi.96@gmail.com>
2025-03-05 11:40:36 -0500

    Add files via upload

---
+++ ecommerce-blog/database/migrations/2025_03_05_143418_create_products_table.php
@@ -0,0 +1,33 @@
+<<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nombre del producto
+            $table->text('description')->nullable(); // Descripción opcional
+            $table->decimal('price', 8, 2); // Precio con 2 decimales
+            $table->string('image')->nullable(); // Imagen opcional
+            $table->foreignId('category_id')->constrained()->onDelete('cascade'); // Relación con categories
+            $table->timestamps(); // Campos created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
+
