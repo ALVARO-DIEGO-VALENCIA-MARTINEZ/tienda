dc29f2c623535114b290d6d1a1d8b04616774598
ALVARO-DIEGO-VALENCIA-MARTINEZ <alvarodi.96@gmail.com>
2025-03-05 11:43:55 -0500

    Add files via upload

---
+++ ecommerce-blog/storage/framework/views/44c0207a3347b9e1a519431c90583f9a.php
@@ -0,0 +1,4 @@
+<footer>
+    <p>&copy; <?php echo e(date('Y')); ?> Mi E-commerce. Todos los derechos reservados.</p>
+</footer>
+<?php /**PATH C:\Users\lauso\Desktop\ecommerce-blog\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
---
+++ ecommerce-blog/storage/framework/views/b44f28e2503a5b7ba2e18caaa6e18d9d.php
@@ -0,0 +1,12 @@
+
+
+<?php $__env->startSection('title', 'Inicio'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <h1>Bienvenido a Mi E-commerce</h1>
+    <p>Descubre nuestros productos y artículos del blog.</p>
+    <a href="<?php echo e(route('products.index')); ?>">Ver tienda</a>
+    <a href="<?php echo e(route('blog.index')); ?>">Leer blog</a>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\lauso\Desktop\ecommerce-blog\resources\views/home.blade.php ENDPATH**/ ?>
