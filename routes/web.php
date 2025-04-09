c190158cd8618a4c471eaa861678ef3852bf19f4
ALVARO-DIEGO-VALENCIA-MARTINEZ <alvarodi.96@gmail.com>
2025-04-09 12:58:53 -0500

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,124 +1,13 @@
 <?php
 
-
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
-
 use App\Http\Controllers\CommentController;
-
-
-
-use Illuminate\Support\Facades\Route;
-
-
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\BlogCategoryController;
-
-Route::resource('articles', ArticleController::class);
-Route::resource('blog-categories', BlogCategoryController::class);
-
-
-
-
-Route::get('/blog', [ArticleController::class, 'index'])->name('blog.index');
-
-
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-
-
-Route::get('/blog', [ArticleController::class, 'index'])->name('blog.index');
-
-
-
-
-
-// Ruta para listar artículos
-Route::get('/blog', [ArticleController::class, 'index'])->name('articles.index');
-
-// Rutas protegidas por autenticación
-Route::middleware(['auth'])->group(function () {
-    Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-    Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
-});
-
-// Ruta para ver un artículo
-Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
-
-
-
-
-// Rutas de artículos
-Route::resource('articles', ArticleController::class);
-
-
-// Página de inicio
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::resource('articles', ArticleController::class);
-
-
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('articles', ArticleController::class);
-});
-
-
-
-
-Route::get('/blog', [ArticleController::class, 'index'])->name('blog.index');
-
-
-
-
-
-
-
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-
-
-
-
-
-
-
-Route::resource('blog', BlogController::class);
-
-
-Route::resource('categories', CategoryController::class);
-
-
-
-
-Route::resource('products', ProductController::class);
-
-
-
-Route::resource('categories', CategoryController::class);
-Route::resource('products', ProductController::class);
-
-
-
-Route::resource('products', ProductController::class);
-Route::resource('categories', CategoryController::class);
-
-
-
-Route::resource('articles', ArticleController::class);
-
-Route::resource('comments', CommentController::class);
-
-
-Route::resource('products', ProductController::class);
-
-
-Route::resource('categories', CategoryController::class);
-
-
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 // Página de inicio
 Route::get('/', function () {
@@ -130,9 +19,45 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-// Rutas para el e-commerce
-Route::resource('products', ProductController::class);
-
 // Rutas para el blog
 Route::resource('blog', BlogController::class);
+Route::resource('blog-categories', BlogCategoryController::class);
 
+
+// Rutas para productos
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit')->middleware('auth');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');
+
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create')->middleware('auth');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show')->middleware('auth');
+Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit')->middleware('auth');
+Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update')->middleware('auth');
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy')->middleware('auth');
+
+// Rutas para artículos
+Route::get('/blog', [ArticleController::class, 'index'])->name('blog.index');
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
+
+// Rutas protegidas por autenticación
+Route::middleware(['auth'])->group(function () {
+    Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+    Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+    Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+    Route::put('/articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
+    Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
+});
+
+// Rutas para comentarios
+Route::resource('comments', CommentController::class);
+
+// Autenticación
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
